6fd0a4051f30ca20c440c634f3422d682f96f6e9
LennyDany-03 <lennydany3@gmail.com>
2025-02-28 09:22:26 +0530

    ver 1.0 File Structure is Ready0

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './NavBar.module.css'
+
+const NavBar = () => {
+  return (
+    <>
+        <h1 className= {styles.NavContainer}>Hello World</h1>
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/NavBar/NavBar'
+import styles from './Home.module.css'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar />
+    </>
+  )
+}
+
+export default Home
